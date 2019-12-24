3b07ed451476080c2fc71da37dcaf8978656eb7c
otu165 <micheu35@gmail.com>
2019-12-24 17:23:20 +0900

    로그인 및 회원가입 뷰 생성

--- app/src/main/java/com/example/sopt/MainActivity.kt
+++ app/src/main/java/com/example/sopt/MainActivity.kt
@@ -1,12 +1,69 @@
 package com.example.sopt
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val BACK_FROM_SIGN_UP_ACTIVITY = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainFunction()
+    }
+
+    private fun mainFunction() {
+        //로그인
+        btnMainLogIn?.setOnClickListener {
+            val id = edtMainId.text.toString()
+            val pwd = edtMainPwd.text.toString()
+
+            //미기입 항목 존재
+            if(id.isEmpty()) {
+                Toast.makeText(this, "아이디를 입력하세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if(pwd.isEmpty()) {
+                Toast.makeText(this, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //정상 로그인
+            Toast.makeText(this, "로그인 완료", Toast.LENGTH_SHORT).show()
+        }
+
+        //회원가입
+        btnMainSignUp?.setOnClickListener {
+            val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+                .putExtra("id", edtMainId.text.toString())
+                .putExtra("pwd", edtMainPwd.text.toString())
+
+            startActivityForResult(intent, BACK_FROM_SIGN_UP_ACTIVITY)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            BACK_FROM_SIGN_UP_ACTIVITY -> {
+                when(resultCode) {
+                    Activity.RESULT_OK -> {
+                        edtMainId.setText(data?.getStringExtra("id"))
+                        edtMainPwd.setText(data?.getStringExtra("pwd"))
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/sopt/SignUpActivity.kt
@@ -0,0 +1,60 @@
+package com.example.sopt
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        initialEditText()
+        signUpFunction()
+    }
+
+    private fun signUpFunction() {
+        //회원가입
+        btnSignUpSignUp.setOnClickListener {
+            val name  = edtSignUpName.text.toString()
+            val id = edtSignUpId.text.toString()
+            val pwd = edtSignUpPwd.text.toString()
+            val pwdChk = edtSignUpPwdChk.text.toString()
+
+            //미입력 항목 존재
+            if(name.isEmpty() || id.isEmpty() || pwd.isEmpty() || pwdChk.isEmpty()) {
+                Toast.makeText(this, "모든 항목을 입력하세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //비밀번호 불일치
+            if(!pwd.equals(pwdChk)) {
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+
+                edtSignUpPwdChk.text = null
+                edtSignUpPwdChk.requestFocus()
+
+                return@setOnClickListener
+            }
+
+            //정상 회원가입
+            val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                .putExtra("id", id)
+                .putExtra("pwd", pwd)
+
+            setResult(Activity.RESULT_OK, intent)
+            this.finish()
+        }
+    }
+
+    //EditText 초기화
+    private fun initialEditText() {
+        edtSignUpId.setText(intent.getStringExtra("id"))
+        edtSignUpPwd.setText(intent.getStringExtra("pwd"))
+    }
+}
