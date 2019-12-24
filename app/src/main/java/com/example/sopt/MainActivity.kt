bd606d75ca5a425572393dbfa83d31e9b94aa5af
otu165 <micheu35@gmail.com>
2019-12-24 21:58:03 +0900

    스크롤뷰 사용

--- app/src/main/java/com/example/sopt/MainActivity.kt
+++ app/src/main/java/com/example/sopt/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.sopt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ScrollView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
