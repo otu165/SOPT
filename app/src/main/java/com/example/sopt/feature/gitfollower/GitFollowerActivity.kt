275aad7dbea3235fc0d5ece4bf72dbd769b97054
otu165 <micheu35@gmail.com>
2019-12-25 02:37:37 +0900

    SnapHelper 적용

--- app/src/main/java/com/example/sopt/feature/gitfollower/GitFollowerActivity.kt
+++ app/src/main/java/com/example/sopt/feature/gitfollower/GitFollowerActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.os.RecoverySystem
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 import com.example.sopt.R
 
 class GitFollowerActivity : AppCompatActivity() {
     private lateinit var recyclerView : RecyclerView
     private lateinit var adapter : GitFollowerAdapter
+
+    private var snapHelper : SnapHelper = LinearSnapHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,9 @@ class GitFollowerActivity : AppCompatActivity() {
         //변수 초기화
         recyclerView = findViewById(R.id.rvGitFollower)
         adapter = GitFollowerAdapter(this)
+
+        //SnapHelper 적용
+        snapHelper.attachToRecyclerView(recyclerView)
 
         //adapter 및 layout 지정
         recyclerView.adapter = adapter
