82001bb1604a18fb1b42f08053dd5215edb48823
otu165 <micheu35@gmail.com>
2019-12-25 15:11:57 +0900

    사용자 이미지 띄움

--- app/src/main/java/com/example/sopt/feature/gitfollower/GitFollowerViewHolder.kt
+++ app/src/main/java/com/example/sopt/feature/gitfollower/GitFollowerViewHolder.kt
@@ -1,17 +1,20 @@
 package com.example.sopt.feature.gitfollower
 
 import android.content.Intent
-import android.media.Image
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.os.StrictMode
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sopt.R
 import com.example.sopt.data.GitFollowerData
 import com.example.sopt.feature.gitrepo.GitRepoActivity
-import kotlinx.android.synthetic.main.rv_follower_item.view.*
+import java.net.URL
+
 
 class GitFollowerViewHolder(view : View) : RecyclerView.ViewHolder(view) {
     private val view : View = view.findViewById(R.id.rvFollowerItem)
@@ -23,7 +26,18 @@ class GitFollowerViewHolder(view : View) : RecyclerView.ViewHolder(view) {
     fun bind(data : GitFollowerData) {
         id.text = data.id
         name.text = data.name
-        img.setImageURI(data.img?.toUri()) //TODO! 오류확인
+
+        if (Build.VERSION.SDK_INT > 9) {
+            val policy =
+                StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+
+            //이미지 등록
+            val url = URL(data.img)
+            val bitmap = BitmapFactory.decodeStream(url.openStream())
+            img.setImageBitmap(bitmap)
+        }
+
 
         view?.setOnClickListener {
             Toast.makeText(view.context, "${id.text} 의 repository를 출력합니다.", Toast.LENGTH_SHORT).show()
